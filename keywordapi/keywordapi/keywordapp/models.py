a77eb817131c3e317576eaa301f44ec0610bfe6c
mihai <mihai@Brasi.(none)>
2013-03-10 11:17:22 +0200

    Added method for getting the number of streams. (#1)

--- keywordapi/keywordapi/keywordapp/models.py
+++ keywordapi/keywordapi/keywordapp/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import User
 class Owner(User):
     stream_number = models.IntegerField(default=15)
 
+    def get_stream_number(self):
+        return self.stream_number
+
--- keywordapi/keywordapi/keywordapp/tests.py
+++ keywordapi/keywordapi/keywordapp/tests.py
@@ -14,9 +14,9 @@ from keywordapi.keywordapp.models import Owner
 class OwnerTest(TestCase):
     def test_default_stream_number(self):
         owner = Owner.objects.create(username='test')
-        self.assertTrue(owner.stream_number==15)
+        self.assertTrue(owner.get_stream_number()==15)
 
     def test_stream_number(self):
         owner = Owner.objects.create(username='test', stream_number=20)
-        self.assertFalse(owner.stream_number==15)
-        self.assertTrue(owner.stream_number==20)
+        self.assertFalse(owner.get_stream_number()==15)
+        self.assertTrue(owner.get_stream_number()==20)
