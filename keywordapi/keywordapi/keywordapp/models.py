c2a5e32aa65de0f329580923149e4ebb7024c396
mihai <mihai@Brasi.(none)>
2013-03-10 11:58:55 +0200

    Added Stream table. (#2)

--- keywordapi/keywordapi/keywordapp/models.py
+++ keywordapi/keywordapi/keywordapp/models.py
@@ -1,3 +1,4 @@
+from tastypie.utils.timezone import now
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -6,4 +7,35 @@ class Owner(User):
 
     def get_stream_number(self):
         return self.stream_number
+
+class Stream(models.Model):
+    owner = models.ForeignKey(Owner)
+    name = models.CharField(max_length=200)
+    date = models.DateField(default=now)
+    language = models.CharField(max_length=200, default='English')
+    location = models.CharField(max_length=200, default='US')
+
+    def get_owner(self):
+        return self.owner
+
+    def get_name(self):
+        return self.name
+
+    def get_location(self):
+        return self.location
+
+    def get_language(self):
+        return self.language
+
+    def set_owner(self, owner):
+        self.owner = owner
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_location(self, location):
+        self.location = location
+
+    def set_language(self, language):
+        self.language = language
 
--- keywordapi/keywordapi/keywordapp/tests.py
+++ keywordapi/keywordapi/keywordapp/tests.py
@@ -1,14 +1,8 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
 import unittest
 import json
 
 from django.test import TestCase
-from keywordapi.keywordapp.models import Owner
+from keywordapi.keywordapp.models import Owner, Stream
 
 
 class OwnerTest(TestCase):
@@ -20,3 +14,53 @@ class OwnerTest(TestCase):
         owner = Owner.objects.create(username='test', stream_number=20)
         self.assertFalse(owner.get_stream_number()==15)
         self.assertTrue(owner.get_stream_number()==20)
+
+
+class StreamTest(TestCase):
+    def setUp(self):
+        self.owner = Owner.objects.create(username='test')
+
+    def test_set_owner(self):
+        stream = Stream.objects.create(owner=self.owner)
+        self.assertTrue(stream.get_owner().id==self.owner.id)
+
+        new_owner = Owner.objects.create(username='test2')
+        stream.set_owner(new_owner)
+        self.assertTrue(stream.get_owner().id==new_owner.id)
+
+    def test_set_name(self):
+        name = 'test1'
+        stream = Stream.objects.create(owner=self.owner, name=name)
+        self.assertTrue(stream.get_name()==name)
+
+        new_name = 'test2'
+        stream.set_name(new_name)
+        self.assertTrue(stream.get_name()==new_name)
+
+    def test_default_location(self):
+        stream = Stream.objects.create(owner=self.owner)
+        self.assertTrue(stream.get_location()=='US')
+
+    def test_set_location(self):
+        location = 'RO'
+        stream = Stream.objects.create(owner=self.owner, location=location)
+        self.assertTrue(stream.get_location()==location)
+
+        new_location = 'UK'
+        stream.set_location(new_location)
+        self.assertTrue(stream.get_location()==new_location)
+
+    def test_default_language(self):
+        stream = Stream.objects.create(owner=self.owner)
+        self.assertTrue(stream.get_language()=='English')
+
+    def test_set_language(self):
+        language = 'Romanian'
+        stream = Stream.objects.create(owner=self.owner, language=language)
+        self.assertTrue(stream.get_language()==language)
+
+        new_language = 'Hindu'
+        stream.set_language(new_language)
+        self.assertTrue(stream.get_language()==new_language)
+
+
