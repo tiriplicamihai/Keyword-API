1e61d92fbd78c4c7fc630bb4daff24df479db0bf
mihai <mihai@Brasi.(none)>
2013-03-14 22:33:28 +0200

    Removed _prepare method and randomized values. (#24)

--- keywordapi/keywordapi/factories.py
+++ keywordapi/keywordapi/factories.py
@@ -3,66 +3,18 @@ import random
 from models import *
 
 class OwnerFactory(factory.Factory):
-    @classmethod
-    def _prepare(cls, create, **kwargs):
-        stream_number = kwargs.pop('stream_number', None)
-        owner = super(OwnerFactory, cls)._prepare(create, **kwargs)
-
-        if stream_number:
-            owner.stream_number = stream_number
-        if create:
-            owner.save()
-        return owner
-
-
     username = factory.LazyAttribute(lambda n: '%030x' % random.randrange(256 ** 15))
+    stream_number = random.randint(0, 60)
 
 
 class StreamFactory(factory.Factory):
-    @classmethod
-    def _prepare(cls, create, **kwargs):
-        owner = kwargs.pop('owner', None)
-        name = kwargs.pop('name', None)
-        language = kwargs.pop('language', None)
-        location = kwargs.pop('location', None)
-        stream = super(StreamFactory, cls)._prepare(create, **kwargs)
-
-        if owner:
-            stream.owner = owner
-        if name:
-            stream.name = name
-        if language:
-            stream.language = language
-        if location:
-            stream.location = location
-        if create:
-            stream.save()
-        return stream
-
-
     owner = factory.SubFactory(OwnerFactory)
     name = factory.LazyAttribute(lambda n: '%030x' % random.randrange(256 ** 15))
-
+    language = factory.LazyAttribute(lambda n: '%030x' % random.randrange(256 ** 15))
+    location = factory.LazyAttribute(lambda n: '%030x' % random.randrange(256 ** 15))
 
 class KeywordFactory(factory.Factory):
-    @classmethod
-    def _prepare(cls, create, **kwargs):
-        stream = kwargs.pop('stream', None)
-        word = kwargs.pop('word', None)
-        key_type = kwargs.pop('key_type', None)
-        keyword = super(KeywordFactory, cls)._prepare(create, **kwargs)
-
-        if stream:
-            keyword.stream = stream
-        if word:
-            keyword.word = word
-        if key_type:
-            keyword.key_type = key_type
-        if create:
-            keyword.save()
-        return keyword
-
-
     stream = factory.SubFactory(StreamFactory)
     word = factory.LazyAttribute(lambda n: '%030x' % random.randrange(256 ** 15))
+    key_type = random.choice(['A', 'N', 'O'])
 
