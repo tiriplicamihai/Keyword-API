61a986f4958f0359da21fa15355c2f83828d7f8f
mihai <mihai@Brasi.(none)>
2013-03-15 23:22:47 +0200

    Added FACTORY_FOR

--- keywordapi/keywordapi/factories.py
+++ keywordapi/keywordapi/factories.py
@@ -7,6 +7,7 @@ class OwnerFactory(factory.Factory):
     """
      Generates random Owner objects.
     """
+    FACTORY_FOR = Owner
     username = factory.LazyAttribute(lambda n: '%030x' % random.randrange(256 ** 15))
     stream_number = random.randint(0, 60)
 
@@ -15,6 +16,7 @@ class StreamFactory(factory.Factory):
     """
      Generates random Stream objects.
     """
+    FACTORY_FOR = Stream
     owner = factory.SubFactory(OwnerFactory)
     name = factory.LazyAttribute(lambda n: '%030x' % random.randrange(256 ** 15))
     language = factory.LazyAttribute(lambda n: '%030x' % random.randrange(256 ** 15))
@@ -24,6 +26,7 @@ class KeywordFactory(factory.Factory):
     """
      Generates random Keyword objects.
     """
+    FACTORY_FOR = Keyword
     stream = factory.SubFactory(StreamFactory)
     word = factory.LazyAttribute(lambda n: '%030x' % random.randrange(256 ** 15))
     key_type = random.choice(KeywordConstants.list_comprehension())
